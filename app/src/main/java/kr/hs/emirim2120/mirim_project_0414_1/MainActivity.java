14c3247bf3b17ea1036befde7387b5943f69be7d
sinheungheung <s2120@e-mirim.hs.kr>
2022-04-19 19:36:14 +0900

    라디오버튼에 따라 이미지 변경

--- app/src/main/java/kr/hs/emirim2120/mirim_project_0414_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim2120/mirim_project_0414_1/MainActivity.java
@@ -25,10 +25,29 @@ public class MainActivity extends AppCompatActivity {
         checkStart = findViewById(R.id.check_start);
         imgv = findViewById(R.id.imgv);
         Button btnDone = findViewById(R.id.btn_done);
+        btnDone.setOnClickListener(btnListener);
 
         // 체크박스를 선택함에 따라 화면에 나타나고 안 나타나는 것을 표시하기 위해서
         checkStart.setOnCheckedChangeListener(checkListener);
     }
+    // 선택한 버튼의 따라 사진이 바뀐다
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        // 선택한 라디오 버튼의 아이디가 반환이 된다
+        public void onClick(View view) {
+            switch(rg.getCheckedRadioButtonId()){
+                case R.id.rb_dog:
+                    imgv.setImageResource(R.drawable.dog);
+                    break;
+                case R.id.rb_cat:
+                    imgv.setImageResource(R.drawable.cat);
+                    break;
+                case R.id.rb_rabbit:
+                    imgv.setImageResource(R.drawable.rabbit);
+                    break;
+            }
+        }
+    };
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
